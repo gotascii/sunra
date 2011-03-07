eb0ebeb809cdefe0e1be9def2d57183217d232dc
Justin Marney <gotascii@gmail.com>
2011-03-07 12:38:55 -0500

    test are passin

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gemspec
 
 group :development do
   gem "minitest", ">= 0"
+  gem "rack-test"
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.5.2"
 end
--- test/helper.rb
+++ test/helper.rb
@@ -14,6 +14,15 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'sunra'
 
 class MiniTest::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    @app ||= self.class.const_get(:App).to_app
+  end
+  
+  def assert_body(body)
+    assert_equal body, last_response.body
+  end
 end
 
 MiniTest::Unit.autorun
